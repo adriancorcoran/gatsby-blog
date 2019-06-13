531c4dca8d6903f25b3d755a5d649e1b4d4dd15e
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-06-13 10:05:00 +0100

    Add Polaris App Provider to wrap root element

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,34 @@
 import React from "react"
-// import Link from "gatsby-link"
-
-import ReactDOM from "react-dom"
 import "@shopify/polaris/styles.css"
-import {
-  AppProvider,
-  Layout,
-  Page,
-  FooterHelp,
-  Card,
-  DisplayText,
-  Link,
-  Button,
-  FormLayout,
-  TextField,
-  AccountConnection,
-  ChoiceList,
-  SettingToggle,
-  Subheading,
-  TextStyle,
-  TextContainer,
-} from "@shopify/polaris"
+import { Page, Card, TextStyle, TextContainer } from "@shopify/polaris"
 
 export default ({ data }) => {
-  const breadcrumbs = [
-    { content: "Sample apps" },
-    { content: "Create React App" },
-  ]
-  const primaryAction = { content: "New product" }
-  const secondaryActions = [{ content: "Import", icon: "import" }]
-
   return (
-    <AppProvider>
-      <Page
-        title="Markdown blog with Gatsby and Shopify Polaris"
-        singleColumn={true}
-      >
-        <TextContainer spacing="tight">
-          <p>{data.allMarkdownRemark.totalCount} Posts</p>
-          <p>&nbsp;</p>
-        </TextContainer>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <Card
-            title={node.frontmatter.title}
-            sectioned
-            key={node.id}
-            secondaryFooterAction={{
-              content: "Read Blog",
-              url: node.fields.slug,
-            }}
-          >
-            <TextContainer spacing="tight">
-              <TextStyle variation="subdued">{node.frontmatter.date}</TextStyle>
-              <p>{node.htmlAst.children[0].children[0].value}</p>
-            </TextContainer>
-          </Card>
-        ))}
-      </Page>
-    </AppProvider>
+    <Page
+      title="Markdown blog with Gatsby and Shopify Polaris"
+      singleColumn={true}
+    >
+      <TextContainer spacing="tight">
+        <p>{data.allMarkdownRemark.totalCount} Posts</p>
+        <p>&nbsp;</p>
+      </TextContainer>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <Card
+          title={node.frontmatter.title}
+          sectioned
+          key={node.id}
+          secondaryFooterAction={{
+            content: "Read Blog",
+            url: node.fields.slug,
+          }}
+        >
+          <TextContainer spacing="tight">
+            <TextStyle variation="subdued">{node.frontmatter.date}</TextStyle>
+            <p>{node.htmlAst.children[0].children[0].value}</p>
+          </TextContainer>
+        </Card>
+      ))}
+    </Page>
   )
 }
 
