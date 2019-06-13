fa0c8f9e6def88bd12b9700332025ebdb1cd04fe
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-06-14 00:02:04 +0100

    Add Polaris wrapper to browser and ssr apis and deployed successfully :)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+import { AppProvider } from "@shopify/polaris"
+
+export const wrapRootElement = ({ element }) => {
+  return <AppProvider>{element}</AppProvider>
+}
